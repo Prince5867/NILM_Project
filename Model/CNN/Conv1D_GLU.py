646eb495d2afb3b4d0c47ec4b1a2b0796c5dda31
Niranjan <niranjankulkarni.fr@gmail.com>
2025-06-12 17:57:51 +0100

    saving model + loading model + better accuracy

--- Model/CNN/Conv1D_GLU.py
+++ Model/CNN/Conv1D_GLU.py
@@ -4,6 +4,11 @@ from keras.layers import Layer, Conv1D, Multiply, Activation, Input, Dense, MaxP
 from keras.models import Model
 import tensorflow as tf
 
+import keras
+from keras.layers import Layer, Add, Conv1D, Multiply
+
+# Register the custom layer for saving/loading without needing custom_objects
+@keras.saving.register_keras_serializable(package="MyLayers")
 class Conv1DGLU(Layer):
     def __init__(self, filters, kernel_size, **kwargs):
         super(Conv1DGLU, self).__init__(**kwargs)
@@ -14,6 +19,21 @@ class Conv1DGLU(Layer):
 
     def call(self, inputs):
         linear_out = self.conv_linear(inputs)
-        # gate_out = tf.keras.activations.sigmoid(self.conv_gate(inputs))
         gate_out = keras.activations.relu(self.conv_gate(inputs), max_value=6) / 6
         return Multiply()([linear_out, gate_out])
+
+    def get_config(self):
+        print("it works")
+        config = super().get_config()
+        config.update({
+            "filters": self.filters,
+            "kernel_size": self.kernel_size
+        })
+        return config
+
+    @classmethod
+    def from_config(cls, config):
+        # config["filters"] = keras.saving.deserialize_keras_object(config["filters"])
+        # config["kernel_size"] = keras.saving.deserialize_keras_object(config["kernel_size"])
+        return cls(**config)
+
--- Model/CNN/ResidualBlock.py
+++ Model/CNN/ResidualBlock.py
@@ -1,23 +1,32 @@
 from keras.layers import Layer, Add, Conv1D
+import keras
 
+@keras.saving.register_keras_serializable(package="MyLayers")
 class ResidualBlock(Layer):
     def __init__(self, sublayer, filters, **kwargs):
-        """
-        Residual block that applies the given sublayer and adds a skip connection.
-        
-        Args:
-            sublayer (Layer): A Keras layer instance (e.g., Conv1DGLU).
-            filters (int): Number of output filters of the sublayer.
-        """
         super(ResidualBlock, self).__init__(**kwargs)
         self.sublayer = sublayer
+        self.filters = filters
         self.projection = Conv1D(filters, kernel_size=1, padding='same')
 
     def call(self, inputs):
         x = self.sublayer(inputs)
-
-        # Match the input to the output if necessary
         if inputs.shape[-1] != x.shape[-1]:
             inputs = self.projection(inputs)
-
         return Add()([x, inputs])
+
+    def get_config(self):
+        config = super().get_config()
+        config.update({
+            # "sublayer": keras.saving.serialize_keras_object(self.sublayer),
+            "sublayer": keras.saving.serialize_keras_object(self.sublayer),
+            "filters": self.filters,
+        })
+        return config
+
+    @classmethod
+    def from_config(cls, config):
+        config["sublayer"] = keras.saving.deserialize_keras_object(config["sublayer"])
+        # config["filters"] = keras.saving.deserialize_keras_object(config["filters"])
+        # sublayer = keras.saving.deserialize_keras_object(config.pop("sublayer"))
+        return cls(**config)
